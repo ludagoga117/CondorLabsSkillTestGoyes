97721287df95509ab71f0d747ad31129ace91d32
Luis David Goyes Garcés <ludagoga117@gmail.com>
2018-06-24 17:24:27 -0500

    Faltaba inicializar el presentador desde la actividad

--- app/src/main/java/com/ldgoyes/condorlabsskilltestgoyes/view/ActivitySplash.java
+++ app/src/main/java/com/ldgoyes/condorlabsskilltestgoyes/view/ActivitySplash.java
@@ -6,20 +6,29 @@ import android.os.Bundle;
 
 import com.ldgoyes.condorlabsskilltestgoyes.R;
 import com.ldgoyes.condorlabsskilltestgoyes.interfaces.InterfaceSplashPresenterView;
+import com.ldgoyes.condorlabsskilltestgoyes.presenter.PresenterSplash;
 
 public class ActivitySplash extends AppCompatActivity implements InterfaceSplashPresenterView {
+
+    private PresenterSplash presenterSplash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        presenterSplash = PresenterSplash.instanceOf(
+                ActivitySplash.this,
+                this
+        );
+        presenterSplash.start();
     }
 
     @Override
     public void launchNextActivity() {
         Intent activityListIntent = new Intent (
                 ActivitySplash.this,
-                ActivityDetail.class
+                ActivityList.class
         );
         activityListIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(activityListIntent);
